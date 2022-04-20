90ebf00889e67ee1df11f5d6c7143ec1417cd001
Jeremy Keeler <j@keeler.dev>
2022-04-19 21:13:43 -0400

    Uncommenting deploy script action and write to version file at very end

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ cd /apps/choralcat
 # Pull latest code
 echo "Pulling latest code from master..."
 git fetch
-echo "Checking out commit $1..."
-git checkout $1
-echo "$1" > .version
+echo "Checking out commit ${VERSION}..."
+git checkout $VERSION
+
 # Pulling latest docker images
 echo "Pulling down latest docker images for version ${VERSION}..."
-docker pull jerkeeler/choralcat:app-$1
-docker pull jerkeeler/choralcat:caddy-$1
+docker pull jerkeeler/choralcat:app-$VERSION
+docker pull jerkeeler/choralcat:caddy-$VERSION
 
 # Shutting down current services
 echo "Shutting down old docker images on version ${oldversion}"
@@ -26,5 +26,8 @@ docker-compose run app .venv/bin/python manage.py migrate
 # Restart the services
 echo "Starting up services on version ${VERSION}"
 docker-compose up -d
+
+# Write new version
+echo "${VERSION}" > .version
 
 echo "Deploy complete!"
